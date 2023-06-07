a83a6ab9d40e23e5081ec8e4f8be27e8c050b1db
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-06-07 04:08:54 -0400

    Feat: create story creation form

---
+++ react-front-end/src/components/form/CreateStoryForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { EditorState } from 'draft-js';
+import * as Yup from "yup";
+import MyEditor from "./MyEditor";
+
+export const CreateStoryForm = () => {
+  return (
+    <Formik
+      //! Image field left out for now to plan out implementation details further
+      initialValues={{
+        title: "placeholder for title",
+        // content: "placeholder for content",
+        editorState: EditorState.createEmpty(), // Initialize the editor state (this state holds the story content)
+      }}
+      validationSchema={Yup.object({
+        title: Yup.string()
+          .max(100, "Must be 100 characters or less")
+          .required("Required"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ values, setFieldValue, handleBlur }) => (
+        <Form>
+          <label htmlFor="title">Title of the Story</label>
+          <Field name="title" type="text" />
+          <ErrorMessage name="title" />
+
+          <label htmlFor="content">Content</label>
+          <MyEditor
+            editorState={values.editorState}
+            onChange={(editorState) =>
+              setFieldValue("editorState", editorState) // Update the editor state
+            }
+            onBlur={handleBlur}
+          />
+
+          {/* Ignoring the buttons for now */}
+          <button type="button">Save</button>
+          <button type="submit">Publish</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
