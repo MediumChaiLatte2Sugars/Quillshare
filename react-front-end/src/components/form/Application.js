fd816ce8166f2983f523134af8498f555a274095
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-06-07 04:09:12 -0400

    Refactor: move application into components

---
+++ react-front-end/src/components/form/Application.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import '../../MyEditor.css';
+import { CreateStoryForm } from './CreateStoryForm';
+import { EditProfileForm } from './EditProfileForm';
+import { PublishForm } from './PublishForm';
+
+export default function Application() {
+
+  return(
+    <>
+      // Test each form by uncommenting the desired form
+      {/* <CreateStoryForm /> */}
+      {/* < EditProfileForm /> */}
+      {/* <PublishForm categories={[
+        { value: 'cateogry 1', label: 'Cateogry 1' },
+        { value: 'category 2', label: 'Category 2' },
+        { value: 'category 3', label: 'Category 3' },
+      ]}/> */}
+    </>
+    )
+
+}
