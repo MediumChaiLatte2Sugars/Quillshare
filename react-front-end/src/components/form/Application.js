05dc24cef37abb71257f7e7bc3c120c823caca67
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-06-09 04:42:33 -0400

    Feat: fetch categories from backend

--- react-front-end/src/components/form/Application.js
+++ react-front-end/src/components/form/Application.js
@@ -1,21 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import '../../MyEditor.css';
 import { CreateStoryForm } from "./CreateStoryForm";
 import { EditProfileForm } from "./EditProfileForm";
 import { PublishSettingsForm } from "./PublishSettingsForm";
 import { PublishForm } from "./PublishForm";
 
+
 export default function Application() {
   return (
     <>
       {/*  Test each form by uncommenting the desired form */}
       {/* <CreateStoryForm /> */}
-      {/* <PublishForm categories={[
-        { value: 'cateogry 1', label: 'Cateogry 1' },
-        { value: 'category 2', label: 'Category 2' },
-        { value: 'category 3', label: 'Category 3' },
-      ]}/> */}
-      < EditProfileForm />
+      <PublishForm />
+      {/* < EditProfileForm /> */}
       {/* <PublishSettingsForm categories={[
         { value: 'cateogry 1', label: 'Cateogry 1' },
         { value: 'category 2', label: 'Category 2' },
--- react-front-end/src/components/form/PublishSettingsForm.js
+++ react-front-end/src/components/form/PublishSettingsForm.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
 
 export const PublishSettingsForm = (props) => {
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("/api/categories");
+        setCategories(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading categories...</div>;
+  }
+
   const initialValues = {
     title: "Title",
-    category: props.categories[0].value,
+    category: categories[0].id,
     tags: "",
     visibility: "public",
   };
@@ -14,8 +36,7 @@ export const PublishSettingsForm = (props) => {
     title: Yup.string()
       .max(100, "Must be 100 characters or less")
       .required("Required"),
-    tags: Yup.string()
-      .required("You need to specify at least 1 tag"),
+    tags: Yup.string().required("You need to specify at least 1 tag"),
     category: Yup.string().required("Required"),
     visibility: Yup.string().required("Required"),
   });
@@ -32,7 +53,12 @@ export const PublishSettingsForm = (props) => {
         <ErrorMessage name="title" component="div" />
 
         <label htmlFor="category">Category</label>
-        <Field name="category" as="select" component={SelectField} options={props.categories} />
+        <Field
+          name="category"
+          as="select"
+          component={SelectField}
+          options={categories}
+        />
         <ErrorMessage name="category" component="div" />
 
         <label htmlFor="tags">Tags</label>
@@ -57,8 +83,8 @@ export const PublishSettingsForm = (props) => {
 const SelectField = ({ field, form, options }) => (
   <select {...field}>
     {options.map((option) => (
-      <option key={option.value} value={option.value}>
-        {option.label}
+      <option key={option.id} value={option.id}>
+        {option.name}
       </option>
     ))}
   </select>
