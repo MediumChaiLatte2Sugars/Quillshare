54c401f9f5d62d8931eb3f3b3324f1621e06d63b
Ozzy Tasci <oguzcantasci@gmail.com>
2023-06-12 18:32:30 -0400

    Add comments for code readability

--- express-back-end/routes/tags.js
+++ express-back-end/routes/tags.js
@@ -8,13 +8,15 @@ router.get('/', (req, res) => {
     .then(tag => {
       const data = {
         tag,
-        message: 'Get all story'
+        message: 'Get all tags'
       }
       res.send(data)
     })
     .catch((err) => console.log('err:', err))
 });
 
+
+// GET a tag by id ------ /api/stories/:id
 router.get('/:id', (req, res) => {
   const tagId = req.params.id;
   Tags.findById(tagId)
@@ -24,6 +26,8 @@ router.get('/:id', (req, res) => {
     .catch((err) => console.log('err:', err));
 });
 
+
+// UPDATE a tag by id ------ /api/tags/:id
 router.put('/:id', (req, res) => {
   const tagId = req.params.id;
   const props = req.body.tag
@@ -34,7 +38,7 @@ router.put('/:id', (req, res) => {
     .catch((err) => console.log('err:', err));
 });
 
-
+// POST a new tag ------ /api/tags
 router.post('/', (req, res) => {
   const props = req.body
   Tags.create(props)
@@ -44,10 +48,9 @@ router.post('/', (req, res) => {
       user
     }))
     .catch((err) => console.log('err:', err))
-
-  
 });
 
+// DELETE a tag by id ------ /api/tags/:id
 router.delete('/:id', (req, res) => {
   const tagId = req.params.id;
   Tags.destroy(tagId)
