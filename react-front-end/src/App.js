df6fbc2d8044883ce92efeed355862b01b242fb5
Pardeep57 <pardeepofficial57@gmail.com>
2023-06-12 17:45:55 -0700

    added new app.js and index.js for easy to understand

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,123 +1,38 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "./App.css";
-import { withAuth0 } from "@auth0/auth0-react";
-import Login from "./components/login";
-import Signup from "./components/signup";
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      categories: [],
-      user: {},
-    };
+      message: 'Click the button to load data!'
+    }
   }
 
-  fetchTest = () => {
-    axios
-      .get("/api/categories") // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        // console.log(response.data); // The entire response from the Rails API
-        this.setState({
-          categories: response.data
-        });
+  fetchData = () => {
+    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+    .then((response) => {
+      // handle success
+      console.log(response.data) // The entire response from the Rails API
+
+      console.log(response.data.message) // Just the message
+      this.setState({
+        message: response.data.message
       });
-  };
-
-  componentDidMount() {
-    const { isAuthenticated } = this.props.auth0;
-
-    if (isAuthenticated) {
-      console.log("User is authenticated");
-      this.fetchData();
-    } else {
-      console.log("User is not authenticated");
-    }
+    }) 
   }
-
-  componentDidUpdate(prevProps) {
-    const { isAuthenticated } = this.props.auth0;
-
-    if (isAuthenticated !== prevProps.auth0.isAuthenticated) {
-      if (isAuthenticated) {
-        console.log("User is authenticated");
-        this.fetchData().then((response) => {
-          this.setState({ user: response });
-        });
-      } else {
-        console.log("User is not authenticated");
-      }
-    }
-  }
-
-  fetchData = async () => {
-    const { user } = this.props.auth0;
-    try {
-      // Check if the user is registered in our database
-      const getUsers = await axios.get(`/api/users`);
-      const filteredUser = getUsers.data.user.find((u) => u.email === user.email);
-  
-      if (!filteredUser) {
-        // User is not registered, perform registration
-        const params = {
-          username: user.nickname,
-          password: user.sub,
-          email: user.email,
-          profile_pic: user.picture,
-          bio: ''
-        };
-  
-        // Register the user
-        const postedUsers = await axios.post(`/api/users`, params);
-        const response = await axios.get(`/api/users/${postedUsers.data.user.id}`);
-        console.log(response);
-        return response.data[0];
-      } else {
-        // User is already registered
-        const response = await axios.get(`/api/users/${filteredUser.id}`);
-        console.log(response);
-        return response.data[0];
-      }
-    } catch (error) {
-      console.error(error);
-      // Throw the error to propagate it to the caller
-      throw error;
-    }
-  };
 
   render() {
-    const { isAuthenticated, logout } = this.props.auth0;
     return (
       <div className="App">
-        <h1>Categories</h1>
-        {
-          this.state.categories.map((c) => 
-            <p>{c.name}</p>
-          )
-        }
-        <button onClick={this.fetchTest}>Fetch Data</button>
-        {isAuthenticated ? (
-          <div>
-            <button
-              onClick={() => logout({ returnTo: window.location.origin })}
-            >
-              Logout
-            </button>
-          <p>
-            {this.state.user.id}
-          </p>
-          </div>
-        ) : (
-          <div>
-            <Login />
-            <Signup />
-          </div>
-        )}
+        <h1>{ this.state.message }</h1>
+        <button onClick={this.fetchData} >
+          Fetch Data
+        </button>        
       </div>
     );
   }
 }
 
-export default withAuth0(App);
+export default App;
---
+++ react-front-end/src/index 11.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
+import './MyEditor.css';
+import Application from './components/form/Application';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Auth0Provider
+  domain="dev-2jggyfjpths0o6ur.us.auth0.com"
+  clientId="kERHuWD6o8BOtyhWE2xSQCEPa9KPhnGq"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+>
+  {/* <App /> auth0  */}
+  <Application /> {/* Forms */}
+</Auth0Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
