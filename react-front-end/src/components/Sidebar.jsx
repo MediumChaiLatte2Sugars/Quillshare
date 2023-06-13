62a07974f658dc7540a1096ceb89f0d45c03d847
Pardeep57 <pardeepofficial57@gmail.com>
2023-06-12 19:15:18 -0700

    added sidebar , rightbar

---
+++ react-front-end/src/components/Sidebar.jsx
@@ -0,0 +1,99 @@
+import {
+    AccountBox,
+    Article,
+    Group,
+    Home,
+    ModeNight,
+    Person,
+    Settings,
+    Storefront,
+  } from "@mui/icons-material";
+  import {
+    Box,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Switch,
+  } from "@mui/material";
+  import React from "react";
+  import {Link} from "react-router-dom";
+  
+
+  const Sidebar = ({mode,setMode}) => {
+    return (
+      <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+        <Box position="fixed">
+          <List>
+            <ListItem disablePadding  component={Link} to="/feed">
+              <ListItemButton component="a" href="#home">
+                <ListItemIcon>
+                  <Home />
+                </ListItemIcon>
+                <ListItemText primary="Homepage" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding >
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemIcon>
+                  <Article />
+                </ListItemIcon>
+                <ListItemText primary="Create Story" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemIcon>
+                  <Group />
+                </ListItemIcon>
+                <ListItemText primary="Notifications" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemIcon>
+                  <Storefront />
+                </ListItemIcon>
+                <ListItemText primary="Explore" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding >
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemIcon>
+                  <Person />
+                </ListItemIcon>
+                <ListItemText primary="Saved Stories" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemIcon>
+                  <Settings />
+                </ListItemIcon>
+                <ListItemText primary="Settings" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding >
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemIcon>
+                  <AccountBox />
+                </ListItemIcon>
+                <ListItemText primary="Profile" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemIcon>
+                  <ModeNight />
+                </ListItemIcon>
+                <Switch onChange={e=>setMode(mode === "light" ? "dark" : "light")}/>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Box>
+    );
+  };
+  
+  export default Sidebar;
