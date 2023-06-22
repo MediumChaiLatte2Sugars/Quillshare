bdbb87cba59eeac1b953575d53fd2bd8786b773d
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-06-22 01:37:02 -0400

    Data: update seeds

--- express-back-end/db/seeds/04_story_categories_seeds.js
+++ express-back-end/db/seeds/04_story_categories_seeds.js
@@ -17,7 +17,52 @@ exports.seed = knex => knex(StoryCategories.tableName).del()
     {
       story_id: 4,
       category_id: 1
+    },
+    {
+      story_id: 3,
+      category_id: 1
+    },
+    {
+      story_id: 4,
+      category_id: 3
+    },
+    {
+      story_id: 5,
+      category_id: 1
+    },
+    {
+      story_id: 6,
+      category_id: 4
+    },
+    {
+      story_id: 7,
+      category_id: 9
+    },
+    {
+      story_id: 8,
+      category_id: 1
+    },
+    {
+      story_id: 9,
+      category_id: 5
+    },
+    {
+      story_id: 10,
+      category_id: 6
+    },
+    {
+      story_id: 11,
+      category_id: 7
+    },
+    {
+      story_id: 12,
+      category_id: 1
+    },
+    {
+      story_id: 13,
+      category_id: 14
     }
-  ])
+  ]
+  )
   .then(sc => Promise.all(sc.map(stories => StoryCategories.create(stories))))
   .catch(err => console.log('err: ', err))
